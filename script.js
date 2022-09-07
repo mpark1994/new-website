90c6bfc3d5c9c39b4c5c9bb946304f8b78317846
mpark1994 <mpark1994@outlook.com>
2022-09-07 01:50:21 -0400

    update

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 // for the answer form
 document.querySelector("#emailsubmit").addEventListener("click", function() {
-    let email = document.querySelector("#exampleFormControlInput1");
     let subject = document.querySelector("#exampleFormControlInput2");
     let message = document.querySelector("#exampleFormControlTextarea1");
 
-    if (email.value == "" || subject.value == "" || message.value == "") {
+    if (subject.value == "" || message.value == "") {
         alert("Please fill out all of the form before submitting!");
         return;
     }
@@ -13,8 +12,8 @@ document.querySelector("#emailsubmit").addEventListener("click", function() {
     message = message.value.split("\n")
     for (let i = 0; i < message.length; i++) {
         message[i] = message[i].replaceAll(" ", "%20")
-        message[i] = `Line${i + 1}-${message[i]}`
     }
+    message = message.join("%0D%0A")
 
-    window.open(`mailto:${email.value}?subject=${subject}&body=${message}`);
+    window.open(`mailto:mpark1994@outlook.com?subject=${subject}&body=${message}`);
 })
